d28474e5394079c62186e76e02c8af052148ec2e
KumarPint00 <90573784+kumarPint00@users.noreply.github.com>
2022-08-29 20:22:42 +0530

    Add files via upload

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,17 +3,21 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const ALCHEMY_API_KEY = "1ylk_Ttc9T4tKKpHDU0cagsOu0-Ycw3f";
-const GOERLI_PRIVATE_KEY = "7521fc8879ad8dd85d930b22ba44c25c94806ed1fa6bcbc9bb2f2d4702127491";
+const PRIVATE_KEY = "d0496dd5e6e8d7d7dbcad827a1c77fe5f3e5ead7d851ece8db388fd4bace00de";
 
 module.exports = {
   solidity: "0.8.9",
   networks:{
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY]
+      accounts: [PRIVATE_KEY]
     },
     hardhat:{
       chainID:31337
+    },
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/JjqID7WJRYIyGCMuSIs9abRB6Hp5Z6zp",
+      accounts: [PRIVATE_KEY]
     }
   }
 };
