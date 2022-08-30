06a9ac9ae6c0f9839e39b735eb7e7eb50f189033
KumarPint00 <90573784+kumarPint00@users.noreply.github.com>
2022-08-30 11:11:39 +0530

    Add files via upload

--- test/VaccineTracker.js
+++ test/VaccineTracker.js
@@ -27,8 +27,29 @@ describe("VaccineTracker", function () {
     );
     await addParticipantTx.wait();
 
-    expect(await vaccineTracker.userName()).to.equal("Pintu");
+    expect(await vaccineTracker.viewParticipant(0)).to.equal( "Pintu",
+    "0x39632F9E3233224804ba2c179D85fDcFd3138B03",
+    "manufacturer");
   });
-  
+  it("Should add new vaccine", async function(){
+    const VaccineTracker = await ethers.getContractFactory("VaccineTracker");
+    const vaccineTracker = await VaccineTracker.deploy();
+    await vaccineTracker.deployed();
+    
+    const addVaccinetTx= await vaccineTracker.registerParticipant(
+      "dolo",
+      "1234",
+      123,
+      1661831815,
+      0
+    );
+    await addVaccinetTx.wait();
+
+    expect(await vaccineTracker.viewVaccine(0)).to.equal("dolo",
+    "1234",
+    123,
+    1661831815,
+    0);
+  });
   
 });
